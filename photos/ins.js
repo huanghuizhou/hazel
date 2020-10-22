3efb2c202bf55e81b7db9d25e4f578c4a62424b8
huanghuizhou <huanghuizhou@uni-ubi.com>
2020-10-22 20:05:15 +0800

    Site updated: 2020-10-22 20:05:13

--- photos/ins.js
+++ photos/ins.js
@@ -116,8 +116,8 @@
         var data = res.list[j].arr;
         var liTmpl = "";
         for (var i = 0, len = data.link.length; i < len; i++) {
-          var minSrc = 'https://raw.githubusercontent.com/huanghuizhou/bolgData/main/min_photos/' + data.link[i];
-          var src = 'https://raw.githubusercontent.com/huanghuizhou/bolgData/main/photos/' + data.link[i];
+          var minSrc = '/assets/min_photos/' + data.link[i];
+          var src = '/assets/photos/' + data.link[i];
           var type = data.type[i];
           var target = src + (type === 'video' ? '.mp4' : '.jpg');
           src += '';
